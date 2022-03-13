b7e7b0fb435c390d08576d56e6db29b654a804bf
ramrusweb <rustamramazanovworkmail@gmail.com>
2022-03-13 13:54:18 +0300

    add: toogle color subscribe button

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+// // change color subcribe button
+const subscribeBtn = document.querySelector('.subscribe-btn')
+
+subscribeBtn.addEventListener('click', () => {
+  subscribeBtn.classList.toggle('active-subscribe-button')
+})
